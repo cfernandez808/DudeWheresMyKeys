063e2c4724330761aaf06301b7cda21c1430f106
Christian Fernandez <tdc027@protonmail.com>
2020-11-22 01:17:29 -0800

    Altitude change

--- DudeWheresMyKeys/ViewController.swift
+++ DudeWheresMyKeys/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  DudeWheresMyKeys
-//
-//  Created by Christian Fernandez on 11/22/20.
-//
 
 import UIKit
+import ARCL
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    
+    var sceneLocationView = SceneLocationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        sceneLocationView.run()
+        view.addSubview(sceneLocationView)
+        
     }
 
 
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sceneLocationView.frame = view.bounds
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        let pingGeolocation = CLLocation(coordinate: CLLocationCoordinate2D(latitude: 36.114704, longitude: -115.201462), altitude: 60)
+        let lostItem = UIImage(named: "keys")!
+        let annotationNode = LocationAnnotationNode(location: pingGeolocation, image: lostItem)
+        
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+
+        
+    }
 }
 
+//You lost your keys in the Las Vegas Strip! This app points on your Camera to where you lost your keys
+//Need to change image to 3D and also allow for walking around the 3D image
+    //Think Mapbox Scenekit SDK with floating astronaut over geolocation...
+//Needs front end component
+//Needs a "receiver" whose sole purpose is to ping the geolocation of your lost item
+//Might be good to look at Mapbox and figure out how to render 3D area of city lost item is in and point out the radius where your item may be 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.1'
+
+target 'DudeWheresMyKeys' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DudeWheresMyKeys
+  pod 'ARCL', '~> 1.3'
+
+end
